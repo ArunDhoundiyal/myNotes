2944385e17cd20a55d2ad10e33d4d9c45379589c
ArunDhoundiyal <151505192+ArunDhoundiyal@users.noreply.github.com>
2024-07-09 22:21:48 +0530

    Update index.js

--- src/components/Note/index.js
+++ src/components/Note/index.js
@@ -1,28 +1,63 @@
 import "./index.css";
-import { IoMdSave } from "react-icons/io";
-import { FaBookmark } from "react-icons/fa";
-import { MdDelete } from "react-icons/md";
-import { MdKeyboardBackspace } from "react-icons/md";
-import { IoShareSocialSharp } from "react-icons/io5";
+import Toggle from "../Toggle";
 import { FaLongArrowAltLeft } from "react-icons/fa";
+import { FiShare2 } from "react-icons/fi";
+import { FaRegBookmark } from "react-icons/fa";
+import { MdDeleteOutline } from "react-icons/md";
+import { FaBold } from "react-icons/fa6";
+import { FaItalic } from "react-icons/fa6";
+import { ImUnderline } from "react-icons/im";
+import { FaStrikethrough } from "react-icons/fa6";
+import { FaSuperscript } from "react-icons/fa6";
+import { FaSubscript } from "react-icons/fa6";
+import { FaListOl } from "react-icons/fa6";
+import { FaListUl } from "react-icons/fa";
+import { FaArrowRotateLeft } from "react-icons/fa6";
+import { FaArrowRotateRight } from "react-icons/fa6";
+import { FaLink } from "react-icons/fa6";
+import { FaAlignLeft } from "react-icons/fa";
+import { FaAlignRight } from "react-icons/fa";
+import { FaAlignCenter } from "react-icons/fa6";
+import { FaAlignJustify } from "react-icons/fa6";
+import { FaIndent } from "react-icons/fa";
+import { FaOutdent } from "react-icons/fa";
+import { MdOutlineEmojiEmotions } from "react-icons/md";
 const Note = () => {
   return (
     <div className="note-bg-container">
-      <div className="top-note-header">
-        <div className="myNotes-note-logo-container">
-          <FaLongArrowAltLeft className="note-back-icon" />
+      <div className="note-header-container">
+        <div className="myNotes-logo-Note-header-container">
+          <FaLongArrowAltLeft />
           <img
             src="https://res.cloudinary.com/da52fiag8/image/upload/v1714148431/ynkdcmb72eeqss8am1dp.png"
-            className="myNotes-note-logo"
-            alt="myNotes-note-logo"
+            className="myNotes-note-header-logo"
+            alt="myNoes-logo"
           />
-          <span className="my-notes-note-text">my-Notes</span>
+          <span className="myNotes-txt">my-Notes</span>
         </div>
-        <div className="note-list-icon-container">
-          <IoMdSave className="note-icon" />
-          <FaBookmark className="note-bookmark-icon" />
-          <MdDelete className="note-icon" />
-          <IoShareSocialSharp className="note-icon" />
+        <FaBold />
+        <FaItalic />
+        <ImUnderline />
+        <FaStrikethrough />
+        <MdOutlineEmojiEmotions />
+        <FaSuperscript />
+        <FaSubscript />
+        <FaListOl />
+        <FaListUl />
+        <FaArrowRotateLeft />
+        <FaArrowRotateRight />
+        <FaLink />
+        <FaAlignLeft />
+        <FaAlignRight />
+        <FaAlignCenter />
+        <FaAlignJustify />
+        <FaIndent />
+        <FaOutdent />
+        <div>
+          <MdDeleteOutline className="note-logo-icon" />
+          <FaRegBookmark className="note-logo-icon" />
+          <FiShare2 className="note-logo-icon" />
+          <Toggle />
         </div>
       </div>
     </div>
